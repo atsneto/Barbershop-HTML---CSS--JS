1f235ec3adf488c8843ae10ea6552c4a2760f206
atsneto <toscanosorrentinonetoa@gmail.com>
2025-11-10 09:37:11 -0300

    Initial Commit

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+var menuIcon = document.querySelector('.menu-icon');
+var ul = document.querySelector('.ul');
+
+menuIcon.addEventListener('click', ()=>{
+    if(ul.classList.contains('ativo')) {
+        ul.classList.remove('ativo');
+        document.querySelector('.menu-icon img').src = "IMG/menu.png";
+    }else{
+        ul.classList.add('ativo');
+        document.querySelector('.menu-icon img').src = "IMG/close.png";
+
+    }
+})
